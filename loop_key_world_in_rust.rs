f48ce0b2857689e4d0b747ac4fb5d1bf157913b9
Mehran Piltan <110996599+MagicalAtom@users.noreply.github.com>
2024-04-20 10:08:19 +0330

    Create loop_key_world_in_rust.rs

---
+++ loop_key_world_in_rust.rs
@@ -0,0 +1,19 @@
+fn main() {
+let mut counter = 1;
+
+loop {
+    counter += 1;
+    if counter <= 120{
+        println!("{counter}");
+        println!("hello");
+    }else {
+        println!("good bye");
+        break;
+    }
+}
+}
+تابع 
+loop
+تا وقتی که با کلمه کلیدی
+break;
+تمام نشده به کار خودش ادامه میده و مثال مناسبی برای تعریف ساختار های شرطی هست
